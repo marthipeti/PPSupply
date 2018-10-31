1e79bf1e95249775b514f925add1d0c4d1be38df
marthipeti <marthipeti1996@gmail.com>
2018-11-01 00:06:31 +0100

    projekt

---
+++ PPSupply/src/main/java/hu/elte/PPSupply/PpSupplyApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.PPSupply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpSupplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpSupplyApplication.class, args);
+	}
+}
---
+++ PPSupply/src/test/java/hu/elte/PPSupply/PpSupplyApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.elte.PPSupply;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PpSupplyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
