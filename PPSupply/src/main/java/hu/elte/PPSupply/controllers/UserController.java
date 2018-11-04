e4936c6f2aba10f0e1b5418b14206244c8094228
marthipeti <marthipeti1996@gmail.com>
2018-11-04 02:28:16 +0100

    controller

--- PPSupply/src/main/java/hu/elte/PPSupply/controllers/UserController.java
+++ PPSupply/src/main/java/hu/elte/PPSupply/controllers/UserController.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hu.elte.PPSupply.controllers;
 
-/**
- *
- * @author ojw1uo
- */
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
     
 }
