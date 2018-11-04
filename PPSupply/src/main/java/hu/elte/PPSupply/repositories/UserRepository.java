985c7c14a42ac2ed27386b5e78e116a43bd33280
marthipeti <marthipeti1996@gmail.com>
2018-11-04 14:03:25 +0100

    controller

--- PPSupply/src/main/java/hu/elte/PPSupply/repositories/UserRepository.java
+++ PPSupply/src/main/java/hu/elte/PPSupply/repositories/UserRepository.java
@@ -2,10 +2,11 @@ package hu.elte.PPSupply.repositories;
 
 import org.springframework.stereotype.Repository;
 import hu.elte.PPSupply.entities.User;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
-    
+    public Optional<User> findByUsername(String username);
 }
