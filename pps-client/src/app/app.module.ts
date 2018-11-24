b77c70b35d7623183f32cb23d2d7c9e6575a21d3
marthipeti <marthipeti1996@gmail.com>
2018-11-25 00:24:56 +0100

    Empty folder

--- pps-client/src/app/app.module.ts
+++ pps-client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    ProfilePageComponent,
+    ProductPageComponent,
+    ContactPageComponent,
+    RegistrationPageComponent
   ],
   imports: [
     BrowserModule,
