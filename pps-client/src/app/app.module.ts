256f1b2e67d6f6af6df3e00c5cf63268ec8910bf
marthipeti <marthipeti1996@gmail.com>
2018-11-25 15:56:35 +0100

    #

--- pps-client/src/app/app.module.ts
+++ pps-client/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { MatToolbarModule,MatButtonModule, MatCardModule, MatGridListModule, MatListModule, MatTableModule, MatSelectModule, MatFormFieldModule } from '@angular/material';
+import { RoutingModule } from './routing/routing.module';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CartPageComponent } from './cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +20,15 @@ import { RegistrationPageComponent } from './registration-page/registration-page
     ProfilePageComponent,
     ProductPageComponent,
     ContactPageComponent,
-    RegistrationPageComponent
+    RegistrationPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ pps-client/src/app/routing/routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Route } from '@angular/router';
+import { ProductPageComponent } from '../product-page/product-page.component';
+import { ProfilePageComponent } from '../profile-page/profile-page.component';
+import { RegistrationPageComponent } from '../registration-page/registration-page.component';
+import { ContactPageComponent } from '../contact-page/contact-page.component';
+import { CartPageComponent } from '../cart-page/cart-page.component';
+
+
+const routes: Route[] = [
+  { path: '', component: ProductPageComponent },
+  { path: 'profile', component: ProfilePageComponent },
+  { path: 'registration', component: RegistrationPageComponent }, 
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'cart', component: CartPageComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
+})
+export class RoutingModule { }
