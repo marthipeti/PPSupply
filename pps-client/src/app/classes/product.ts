3e8dfad1f3cecc79c894e85ada032289049e97e2
marthipeti <marthipeti1996@gmail.com>
2018-12-02 23:32:40 +0100

    Remove duplicated directory

--- pps-client/src/app/classes/product.ts
+++
@@ -1,9 +0,0 @@
-import { Tag } from "./tag";
-
-export class Product {
-    public id: number;
-    public name: string;
-    public quantity: number;
-    public description: string;
-    public tags: Tag[];
-}
--- pps-client/src/app/classes/user.ts
+++
@@ -1,6 +0,0 @@
-export class User {
-    public id: number;
-    public name: string;
-    public userName: string;
-    public role: string;
-}
